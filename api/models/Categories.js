dd5eeebb3c6b32b5a4d8dadf06f6dbd9b338775c
ana.araujo <ana.araujo@student.vinci.be>
2023-12-05 23:43:19 +0100

    add category with category title unique , delete category : ON API

--- api/models/Categories.js
+++ api/models/Categories.js
@@ -1,23 +1,76 @@
-const { parse } = require('../utils/json');
+const path = require('node:path');
+const { serialize, parse } = require('../utils/json');
 
-const CAT = [
-  {
-    id: 1,
-    title: 'Sports',
-  },
-  {
-    id: 2,
-    title: 'Technologies',
-  },
-  {
-    id: 3,
-    title: 'Musique',
-  },
-];
+const jsonDbPath = path.join(__dirname, '/../data/categories.json');
 
-function getAllCategories() {
-  const categories = parse(CAT);
-  return categories;
+const categoriesTable = [];
+
+function readAllCategories(orderBy) {
+  const orderByTitle = orderBy?.includes('title') ? orderBy : undefined;
+  let orderedCategoriesTable;
+  const categories = parse(jsonDbPath, categoriesTable);
+  // eslint-disable-next-line max-len
+  if (orderByTitle) orderedCategoriesTable = [...categories].sort((a, b) => a.title.localeCompare(b.title));
+  if (orderByTitle === '-title') orderedCategoriesTable = orderedCategoriesTable.reverse();
+
+  const allCategoriesPotentiallyOrderd = orderedCategoriesTable ?? categories;
+  return allCategoriesPotentiallyOrderd;
 }
 
-module.exports = { getAllCategories };
+function createCategory(title) {
+  const categories = parse(jsonDbPath, categoriesTable);
+
+  console.log('TOPICS', categories);
+
+  const newCategory = {
+    id: getNextId(),
+    title,
+    creationDate: getDateNow(),
+  };
+
+  categories.push(newCategory);
+
+  serialize(jsonDbPath, categories);
+
+  return newCategory;
+}
+function getNextId() {
+  const categories = parse(jsonDbPath, categoriesTable);
+
+  const lastItemIndex = categories?.length !== 0 ? categories.length - 1 : undefined;
+
+  if (lastItemIndex === undefined || lastItemIndex === null) return 1;
+
+  const lastId = categories[lastItemIndex]?.id;
+
+  const nextId = lastId + 1;
+
+  return nextId;
+}
+
+function getDateNow() {
+  const date = new Date();
+  return `Ajouté le ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} à  ${date.getHours()}:${date.getMinutes()} `;
+}
+function deleteCategory(id) {
+  const idAsNumber = Number(id);
+  const categories = parse(jsonDbPath);
+  const foundIndex = categories.findIndex((category) => category.id === idAsNumber);
+  if (foundIndex < 0) return undefined;
+  const deletedCategories = categories.splice(foundIndex, 1);
+  const deletedCategorie = deletedCategories[0];
+  serialize(jsonDbPath, categories);
+
+  return deletedCategorie;
+}
+function isTitleAlreadyExists(title) {
+  const categories = parse(jsonDbPath, categoriesTable);
+  return categories.some((category) => category.title.toLowerCase() === title.toLowerCase());
+}
+
+module.exports = {
+  createCategory,
+  readAllCategories,
+  deleteCategory,
+  isTitleAlreadyExists,
+};
--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,12 +1,38 @@
 const express = require('express');
-const { getAllCategories } = require('../models/Categories');
+
+const {
+  readAllCategories, createCategory, deleteCategory, isTitleAlreadyExists,
+} = require('../models/categories');
 
 const router = express.Router();
 
+// read all categories
 router.get('/', (req, res) => {
-  const allCategories = getAllCategories();
+  const allCategoriesPotentiallyOrdered = readAllCategories(req?.query?.order);
 
-  return res.json(allCategories);
+  return res.json(allCategoriesPotentiallyOrdered);
 });
 
+// create topic
+router.post('/', (req, res) => {
+  const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+
+  console.log('TITLE', title);
+
+  if (!title) return res.sendStatus(404);
+
+  const isExists = isTitleAlreadyExists(title);
+  if (isExists) return res.sendStatus(409);
+  const categoryCreated = createCategory(title);
+
+  return res.json(categoryCreated);
+});
+
+router.delete('/:id', (req, res) => {
+  const deletedCategory = deleteCategory(req.params.id);
+
+  if (!deletedCategory) return res.sendStatus(404);
+
+  return res.json(deletedCategory);
+});
 module.exports = router;
