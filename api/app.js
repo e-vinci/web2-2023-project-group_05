a5ce57b78510e6b9d747406636f169bdc2f1bcd8
marwan <marwan.benali@student.vinci.be>
2023-12-16 17:20:54 +0100

    add : permision for url frontend for production

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:8080', 'https://e-baron.github.io'],
+  origin: ['http://localhost:8080', 'https://e-baron.github.io', 'https://group-05.github.io/web2-2023-project-group_05/'],
 };
 
 const pizzaRouter = require('./routes/pizzas');
